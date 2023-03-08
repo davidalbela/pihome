e19610c6824386829a6bddd7e092861e4b8034fa
David Albela Pérez <3659067+davidalbela@users.noreply.github.com>
2023-03-08 01:14:07 +0100

    Add optional TODOs for movement sensor PIR

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -9,7 +9,12 @@ GPIO.setmode(GPIO.BOARD)
 sensor = Adafruit_DHT.DHT11
 
 # TODO 
-sensor_pin = # Indicar el pin GPIO donde se conecta el sensor
+sensor_pin = # Indicar el pin GPIO donde se conecta el sensor DHT11
+
+# TODO (Opcional)
+# motion_pin = # Indicar el pin de la variable motions del sensor PIR
+# GPIO.setup(motion_pin, GPIO.IN)
+
 
 # Add webserver
 from flask import Flask
@@ -18,7 +23,10 @@ app = Flask(__name__)
 @app.route('/metrics')
 def metrics():
     umid, temp = Adafruit_DHT.read_retry(sensor, sensor_pin)
+    # TODO  (Opcional)
+    # motion = GPIO.input(motion_pin)
     if umid is not None and temp is not None:
+        # TODO (Opcional) add "pihome_movement {} \n" to the returned text with the integer variable motions
         return '# HELP local_temp local temperature\n# TYPE local_temp gauge\nlocal_temp {}\n# HELP local_humidity local humidity\n# TYPE local_humidity gauge\nlocal_humidity {}\n'.format(int(temp), int(umid)), 200, {'Content-Type': 'text/plain; charset=utf-8'}
     else:
         return 'Could not read from DHT11.', 200, {'Content-Type': 'text/plain; charset=utf-8'}
